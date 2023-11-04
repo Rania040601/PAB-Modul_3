68b2e6aa842dd32d1ce2ed591d3ed4d40230c3bf
Rania040601 <133231186+Rania040601@users.noreply.github.com>
2023-11-04 21:29:22 +0700

    Add files via upload

---
+++ screens/list.js
@@ -0,0 +1,143 @@
+import {
+  FlatList,
+  Image,
+  Text,
+  Modal,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Button,
+  Alert,
+} from "react-native";
+
+// Dummmy Data (Array of Object)
+const datas = [
+  {
+    id: 1,
+    title:
+      "Dorong Pengembangan UMKM, IT Telkom Surabaya Gandeng UD Rozi dalam Pembuatan Website",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/Salinan-IMG_0823-768x512.jpg",
+  },
+  {
+    id: 2,
+    title: "Mahasiswa ITTelkom Surabaya Menang Kompetisi di New Delhi",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghjk-768x492.jpg",
+  },
+  {
+    id: 3,
+    title:
+      "Benarkah Generasi Baru Generasi Strawberi? Ayo Belajar Growth Mindset dari Kampus ITTelkom Surabaya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghkl-768x472.jpg",
+  },
+  {
+    id: 4,
+    title: "4 Industri Tahan Resesi! Pastikan Kemampuanmu Termasuk Di Dalamnya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/mbacay-768x512.jpg",
+  },
+  {
+    id: 5,
+    title:
+      "Selamat Datang Mahasiswa Baru, PKKMB ITTelkom Surabaya Berlangsung Secara Luring",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/09/DSC_1931-768x512.jpg",
+  },
+  {
+    id: 6,
+    title:
+      "Dorong Pengembangan UMKM, IT Telkom Surabaya Gandeng UD Rozi dalam Pembuatan Website",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/Salinan-IMG_0823-768x512.jpg",
+  },
+  {
+    id: 7,
+    title: "Mahasiswa ITTelkom Surabaya Menang Kompetisi di New Delhi",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghjk-768x492.jpg",
+  },
+  {
+    id: 8,
+    title:
+      "Benarkah Generasi Baru Generasi Strawberi? Ayo Belajar Growth Mindset dari Kampus ITTelkom Surabaya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghkl-768x472.jpg",
+  },
+  {
+    id: 9,
+    title: "4 Industri Tahan Resesi! Pastikan Kemampuanmu Termasuk Di Dalamnya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/mbacay-768x512.jpg",
+  },
+  {
+    id: 10,
+    title:
+      "Selamat Datang Mahasiswa Baru, PKKMB ITTelkom Surabaya Berlangsung Secara Luring",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/09/DSC_1931-768x512.jpg",
+  },
+];
+
+// Functional Component
+const List = () => {
+  // Arrow Function with destructured argument
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.view} onPress={() => alert("Pressed")}>
+        <View>
+          <Image source={{ uri: item.image }} style={styles.image} />
+          <Text style={styles.text}>{item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderModal = (selectedItem) => {
+    return (
+      <Modal
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+        transparent={true}
+      >
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <Image source={{ uri: selectedItem.image }} style={styles.image} />
+            <Text style={styles.modalText}>{selectedItem.title}</Text>
+            <Button title="Lanjut Membaca" style={styles.showAlertButton} onPress={showAlert} />
+            <Button title="Tutup" color="#E9B824" style={styles.closeButton} onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
+    );
+
+  };
+  return (
+    <FlatList
+      data={datas}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+
+  // Styles
+  const styles = StyleSheet.create({
+    view: {
+      padding: 15,
+      borderBottomColor: "#dddddd",
+      borderBottomWidth: 1,
+    },
+    image: {
+      height: 200,
+      width: null,
+    },
+    text: {
+      fontSize: 18,
+      paddingTop: 10,
+    },
+  });
+
+  export default List;
